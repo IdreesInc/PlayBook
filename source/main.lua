b1e9182867b56c0012a45fa546a1ab59702f34af
Idrees Hassan <idrees@idreesinc.com>
2023-10-14 17:00:20 -0400

    Smoothly scroll a significant amount of text

--- source/main.lua
+++ source/main.lua
@@ -2,34 +2,82 @@ local graphics = playdate.graphics
 -- 50 Hz is max refresh rate
 playdate.display.setRefreshRate(50)
 
--- Read something from the filesystem
+-- Constants
+local MAX_FILE_SIZE = 4 * 1024 * 1024
+local MARGIN = 10
+local DEVICE_WIDTH = 400
+local DEVICE_HEIGHT = 240
 
-local file = playdate.file.open("test.txt")
-local MAX_SIZE = 4 * 1024 * 1024;
-local text = file:read(MAX_SIZE)
-print(text)
+-- Variables
+local offset = 0;
+local lines = {}
+local lineHeight = 0
 
--- Load the font
-local font = graphics.font.new("fonts/Roobert-11-Medium")
-assert(font)
-graphics.setFont(font)
+function init()
+	-- Load the font
+	local font = graphics.font.new("fonts/Roobert-11-Medium")
+	assert(font)
+	graphics.setFont(font)
 
-graphics.setBackgroundColor(graphics.kColorWhite)
-graphics.drawText("Crank it up", 10, 0)
-graphics.drawText("Lorem ipsum dolor sit amet, consectetur", 10, 25)
--- print(graphics.getTextSize("Lorem ipsum dolor sit amet, consectetur"))
-graphics.drawText("adipiscing elit, sed do eiusmod tempor", 10, 50)
-graphics.drawText("incididunt ut labore et dolore magna aliqua.", 10, 75)
-graphics.drawText("---", 10, 100)
-graphics.drawText("Lorem ipsum dolor sit amet, consectetur", 10, 125)
-graphics.drawText("adipiscing elit, sed do eiusmod tempor", 10, 150)
-graphics.drawText("incididunt ut labore et dolore magna aliqua.", 10, 175)
+	-- Read something from the filesystem
+	local file = playdate.file.open("test.txt")
+	local text = file:read(MAX_FILE_SIZE)
+	assert(text)
 
-function playdate.update()
-	-- Required for some reason
+	-- Split the text into lines
+	lines = splitText(text)
+	lineHeight = graphics.getTextSize("A") * 1.5
+
+	-- Set the background color
+	graphics.setBackgroundColor(graphics.kColorWhite)
 end
 
--- -- Callbacks
+-- Update loop
+function playdate.update()
+	updateText()
+end
+
+-- Split text into lines with a maximum width of 400 - 2 * MARGIN
+function splitText(text)
+	local lines = {}
+	local line = ""
+	local maxWidth = DEVICE_WIDTH - 2 * MARGIN
+	for word in string.gmatch(text, "%S+") do
+		if line == "" then
+			-- First word
+			line = word
+		elseif graphics.getTextSize(line .. " " .. word) > maxWidth then
+			-- Line is too long, commit it and start again with this word
+			table.insert(lines, line)
+			print(graphics.getTextSize(line))
+			line = word
+		else
+			line = line .. " " .. word
+		end
+	end
+	table.insert(lines, line)
+	return lines
+end
+
+-- Print the text
+function updateText()
+	graphics.clear()
+	graphics.drawText(playdate.getCrankPosition(), MARGIN, offset)
+	-- Only draw the lines that are visible
+	local start = math.max(math.floor(-offset / lineHeight), 1)
+	local stop = math.min(start + math.floor(DEVICE_HEIGHT / lineHeight), #lines)
+	local flooredOffset = math.floor(offset)
+	for i = start, stop do
+		graphics.drawText(lines[i], MARGIN, flooredOffset + i * lineHeight)
+	end
+end
+
+-- Register input callbacks
+function playdate.cranked(change, acceleratedChange)
+	-- print("cranked", change, acceleratedChange)
+	offset = offset + change
+end
+
 function playdate.upButtonDown()
 	print("up")
 end
@@ -53,3 +101,5 @@ end
 function playdate.BButtonDown()
 	print("B")
 end
+
+init()
