cbcfedf8e5d9e7531c10ddd2c436ae9c512fe28d
Idrees Hassan <idrees@idreesinc.com>
2023-10-14 14:56:16 -0400

    Add fonts

--- source/main.lua
+++ source/main.lua
@@ -1,14 +1,21 @@
-local gfx = playdate.graphics
+local graphics = playdate.graphics
 playdate.display.setRefreshRate(60)
 
-gfx.setBackgroundColor(gfx.kColorWhite)
-gfx.drawText("Lorem ipsum dolor sit amet, consectetur", 10, 25)
-gfx.drawText("adipiscing elit, sed do eiusmod tempor", 10, 50)
-gfx.drawText("incididunt ut labore et dolore magna aliqua.", 10, 75)
+-- set the font
+local font = graphics.font.new("fonts/Roobert-11-Medium")
+assert(font)
+graphics.setFont(font)
 
-gfx.drawText("Lorem ipsum dolor sit amet, consectetur", 10, 125)
-gfx.drawText("adipiscing elit, sed do eiusmod tempor", 10, 150)
-gfx.drawText("incididunt ut labore et dolore magna aliqua.", 10, 175)
+graphics.setBackgroundColor(graphics.kColorWhite)
+graphics.drawText("Crank it up", 10, 0)
+graphics.drawText("Lorem ipsum dolor sit amet, consectetur", 10, 25)
+-- print(graphics.getTextSize("Lorem ipsum dolor sit amet, consectetur"))
+graphics.drawText("adipiscing elit, sed do eiusmod tempor", 10, 50)
+graphics.drawText("incididunt ut labore et dolore magna aliqua.", 10, 75)
+graphics.drawText("---", 10, 100)
+graphics.drawText("Lorem ipsum dolor sit amet, consectetur", 10, 125)
+graphics.drawText("adipiscing elit, sed do eiusmod tempor", 10, 150)
+graphics.drawText("incididunt ut labore et dolore magna aliqua.", 10, 175)
 
 function playdate.update()
 	-- Required for some reason
