c76c6eb95e1971d60c62e99dea3e1ce65f05130c
Idrees Hassan <idrees@idreesinc.com>
2023-10-15 11:48:27 -0400

    Work in progress text splitting

--- source/main.lua
+++ source/main.lua
@@ -4,7 +4,7 @@ playdate.display.setRefreshRate(50)
 
 -- Constants
 local MAX_FILE_SIZE = 4 * 1024 * 1024
-local MARGIN = 10
+local MARGIN = 30
 local DEVICE_WIDTH = 400
 local DEVICE_HEIGHT = 240
 
@@ -20,7 +20,7 @@ function init()
 	graphics.setFont(font)
 
 	-- Read something from the filesystem
-	local file = playdate.file.open("test.txt")
+	local file = playdate.file.open("rough.txt")
 	local text = file:read(MAX_FILE_SIZE)
 	assert(text)
 
@@ -39,24 +39,38 @@ end
 
 -- Split text into lines with a maximum width of 400 - 2 * MARGIN
 function splitText(text)
+	print("Splitting text...")
 	local lines = {}
 	local line = ""
 	local maxWidth = DEVICE_WIDTH - 2 * MARGIN
-	for word in string.gmatch(text, "%S+") do
-		if line == "" then
+	local splitty = split(text)
+	for i = 1, 10000 do
+		local word = splitty(i)
+		if word == "{newline}" then
+			-- Newline
+			table.insert(lines, line)
+			line = ""
+		elseif line == "" then
 			-- First word
 			line = word
 		elseif graphics.getTextSize(line .. " " .. word) > maxWidth then
 			-- Line is too long, commit it and start again with this word
 			table.insert(lines, line)
-			print(graphics.getTextSize(line))
 			line = word
 		else
 			line = line .. " " .. word
 		end
 	end
 	table.insert(lines, line)
+	print("Split into " .. #lines .. " lines")
 	return lines
+end
+
+function split(text)
+	-- replace \n with "{newline}"
+	local newText = string.gsub(text, "\n", " {newline} ")
+	-- split on spaces
+	return string.gmatch(newText, "%S+")
 end
 
 -- Print the text
