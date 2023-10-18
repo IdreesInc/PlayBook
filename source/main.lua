59dedb1fbfea140a1a7375caa749462c783e3d48
Idrees Hassan <idrees@idreesinc.com>
2023-10-17 22:16:20 -0400

    Add option to remove border

--- source/main.lua
+++ source/main.lua
@@ -6,13 +6,14 @@ playdate.display.setRefreshRate(50)
 
 -- Constants
 local MAX_FILE_SIZE = 4 * 1024 * 1024
-local MARGIN = 24
 local DEVICE_WIDTH = 400
 local DEVICE_HEIGHT = 240
 local VOLUME_ACCELERATION = 0.05
 local MAX_VOLUME = 0.025
 local CRANK_SCROLL_SPEED = 1.5
 local BTN_SCROLL_SPEED = 5
+local MARGIN_WITH_BORDER = 24
+local MARGIN_WITHOUT_BORDER = 10
 
 -- Variables
 local offset = 0;
@@ -22,6 +23,9 @@ local lineHeight = 0
 local inverted = false
 local directionHeld = 0
 local pattern = graphics.image.new("pattern")
+local showBorder = false
+local margin = 10
+local sourceText = nil
 
 function init()
 	-- Load the font
@@ -31,11 +35,11 @@ function init()
 
 	-- Read something from the filesystem
 	local file = playdate.file.open("rough.txt")
-	local text = file:read(MAX_FILE_SIZE)
-	assert(text)
+	sourceText = file:read(MAX_FILE_SIZE)
+	assert(sourceText)
 
 	-- Split the text into lines
-	lines = splitText(text)
+	lines = splitText(sourceText)
 	lineHeight = graphics.getTextSize("A") * 1.6
 
 	-- Set the background color
@@ -58,19 +62,21 @@ end
 
 function drawText()
 	graphics.clear()
-	graphics.drawText(playdate.getCrankPosition(), MARGIN, offset)
+	graphics.drawText(playdate.getCrankPosition(), margin, offset)
 	-- Only draw the lines that are visible
 	local start = math.max(math.floor(-offset / lineHeight), 1)
 	local stop = math.min(start + math.floor(DEVICE_HEIGHT / lineHeight) + 1, #lines)
 	local flooredOffset = math.floor(offset)
 	for i = start, stop do
 		local y = flooredOffset + i * lineHeight
-		graphics.drawText(lines[i], MARGIN, y)
+		graphics.drawText(lines[i], margin, y)
 	end
 	local patternMargin = 0
-	for i=-1, math.ceil(DEVICE_HEIGHT / pattern.height) do
-		pattern:draw(patternMargin, i * pattern.height + flooredOffset % pattern.height)
-		pattern:draw(DEVICE_WIDTH - pattern.width - patternMargin, i * pattern.height + flooredOffset % pattern.height, -1)
+	if showBorder then
+		for i=-1, math.ceil(DEVICE_HEIGHT / pattern.height) do
+			pattern:draw(patternMargin, i * pattern.height + flooredOffset % pattern.height)
+			pattern:draw(DEVICE_WIDTH - pattern.width - patternMargin, i * pattern.height + flooredOffset % pattern.height, -1)
+		end
 	end
 end
 
@@ -79,7 +85,7 @@ function splitText(text)
 	print("Splitting text...")
 	local lines = {}
 	local line = ""
-	local maxWidth = DEVICE_WIDTH - 2 * MARGIN
+	local maxWidth = DEVICE_WIDTH - 2 * margin
 	local splitty = split(text)
 	for i = 1, 10000 do
 		local word = splitty(i)
@@ -144,6 +150,13 @@ end
 
 function playdate.AButtonDown()
 	print("A")
+	showBorder = not showBorder
+	if showBorder then
+		margin = MARGIN_WITH_BORDER
+	else
+		margin = MARGIN_WITHOUT_BORDER
+	end
+	lines = splitText(sourceText)
 end
 
 function playdate.BButtonDown()
